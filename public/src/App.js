d29f4e21a7620219ec9c23482b7a8360d26e266c
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-06-10 14:24:33 +0530

    routes added

--- public/src/App.js
+++ public/src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Chat from "./pages/Chat";
+import Login from "./pages/Login";
 import Register from "./pages/Register";
 
 const App = () => {
@@ -7,6 +9,8 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Chat />} />
       </Routes>
     </BrowserRouter>
   );
