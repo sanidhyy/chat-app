d98a26431211e49741023bae714f7acb3a83f400
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-06-15 13:37:20 +0530

    added new environment variables

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5000";
+export const host = proce.env.REACT_APP_SERVER_URI;
 
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ const server = app.listen(process.env.PORT, () => {
 
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: process.env.CLIENT_URL,
     credentials: true,
   },
 });
